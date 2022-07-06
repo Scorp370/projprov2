d1ba3d8bce2eaac03ecf3f3d70c980a7829e0070
Cedric.C <kmuced@gmail.com>
2022-07-06 15:12:02 +0200

    second commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,18 +7,25 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div className='contact'>
-            <form name='contact' className='contact_form' data-netlify="true" onSubmit="submit">
+
+            <form
+             name='contact'
+             method="post" 
+             className='contact_form' 
+             data-netlify="true" 
+             onSubmit="submit">
+
                 <label for='name'>Nom</label>
                 <input type='text' name='name'/>
 
                 <input type="hidden" name="form-name" value="contact" />
-                <label for='name'>Email</label>
+                <label for='email'>Email</label>
                 <input type='email' name='email'/>
 
                 <label for='message'>Message</label>
                 <textarea name='message'></textarea>
 
-                <button type='button'>Envoyer</button>
+                <button type='submit'>Envoyer</button>
 
 
 
